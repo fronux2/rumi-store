120b4223871f688950a03a5d3118a9d9b00f18f0
marcos <marcosantonio.munozg@gmail.com>
2023-10-06 00:47:42 -0300

    corrigiendo css

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -13,36 +13,36 @@ import { Navigation, Autoplay } from 'swiper/modules'
 export default function Carousel () {
   return (
     <>
-      <Swiper loop={true} navigation={true} autoplay={{ delay: 13000, disableOnInteraction: false }} modules={[Navigation, Autoplay]} className="mySwiper">
+      <Swiper loop={true} navigation={true} autoplay={{ delay: 5600, disableOnInteraction: false }} modules={[Navigation, Autoplay]} className="mySwiper">
 
             <SwiperSlide>
-                <div className='flex col relative w-full h-[600px]' >
+                <div className='flex col relative w-full h-[600px] bg-slate-200' >
                     <Image src='https://sallobos.cl/content/uploads/bfi_thumb/Header_Inicio_1440x800_Lobos-ny064syqelzrmursoohpk0m25tb65w8126da8dsxkw.jpg' alt="" className='imagen-recortada h-full w-full' width={600} height={600}/>
-                    <div className='duration-500'>
-                        <h1>El Sabor de Nuestro Hogar</h1>
-                        <h1>Desde hace más de 100 años en la mesa de todo Chile</h1>
+                    <div className='pt-44 pl-6'>
+                        <h1 className='font-bold text-4xl'>El Sabor de Nuestro Hogar</h1>
+                        <h1 className='pt-2'>Desde hace más de 100 años en la mesa de todo Chile</h1>
                     </div>
                     <Image src='https://sallobos.cl/content/uploads/bfi_thumb/2-nqzlgqlwg365yyspn16bjq7gxnm2fk9xa4ttg7shs0.png' alt='' className='w-40 h-40 absolute top-[50%] left-[40%] translateImg' width={600} height={600}/>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
-                <div className='flex col relative w-full' >
-                    <Image src='https://sallobos.cl/content/uploads/bfi_thumb/marcela-plato-scaled-p62es1lqeqd5gk5mvvod2tbvfw92u6nyhasokh83r4.jpg' alt="" className='imagen-recortada' width={600} height={600}/>
-                    <div>
-                        <h1>Mira nuestra serie</h1>
-                        <h1>Por que una receta con historia, siempre tiene más sabor.</h1>
-                        <div>CONOCE MÁS</div>
+                <div className='flex col relative w-full bg-slate-200' >
+                    <Image src='https://sallobos.cl/content/uploads/bfi_thumb/marcela-plato-scaled-p62es1lqeqd5gk5mvvod2tbvfw92u6nyhasokh83r4.jpg' alt="" className='imagen-recortada h-full w-full' width={600} height={600}/>
+                    <div className='pt-44 pl-6 '>
+                        <h1 className='font-bold text-4xl'>Mira nuestra serie</h1>
+                        <h1 className='pt-2 pb-2'>Por que una receta con historia, siempre tiene más sabor.</h1>
+                        <h1 className='p-2 mt-4 pl-6 rounded-3xl bg-red-600 w-40 text-white'>CONOCE MÁS</h1>
                     </div>
                     <Image src='https://sallobos.cl/content/uploads/bfi_thumb/iconito-p62ep90ijy44g2cbfu85n44u4miwjd45iixvquwpog.png' alt='' className='w-40 h-40 absolute top-[50%] left-[40%] translateImg' width={600} height={600}/>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
-                <div className='flex col relative w-full' >
-                    <Image src='https://sallobos.cl/content/uploads/bfi_thumb/home-selecciones-ppn1slpe9xigffyoy161lt19p7sfpj7y7swh1rv11c.jpg' alt="" className='imagen-recortada' width={600} height={600}/>
-                    <div>
-                        <h1>Lobos Selecciones</h1>
-                        <h1>sales de especialidad para disfrutar una experiencia en sabor.</h1>
-                        <div>CONOCE MÁS</div>
+                <div className='flex col relative w-full bg-slate-200' >
+                    <Image src='https://sallobos.cl/content/uploads/bfi_thumb/home-selecciones-ppn1slpe9xigffyoy161lt19p7sfpj7y7swh1rv11c.jpg' alt="" className='imagen-recortada h-full w-full' width={600} height={600}/>
+                    <div className='pt-44 pl-6'>
+                        <h1 className='font-bold  text-4xl'>Lobos Selecciones</h1>
+                        <h1 className='pt-2 pb-2'>sales de especialidad para disfrutar una experiencia en sabor.</h1>
+                        <h1 className='p-2 mt-4 pl-6 rounded-3xl bg-red-600 w-40 text-white'>CONOCE MÁS</h1>
                     </div>
                     <Image src='https://sallobos.cl/content/uploads/bfi_thumb/home-logoselecciones-ppn1smmqx94e8u1x17ljne80jk5wi0v5mzi421d4ao.png' alt='' className='w-40 h-40 absolute top-[50%] left-[40%] translateImg' width={600} height={600}/>
                 </div>
--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -7,23 +7,26 @@ export default function PageIndex () {
   return (
     <section className='bg-white'>
       <NavPage>NAV</NavPage>
-      <Carousel/>
-      <div className='bg-slate-200 px-5 py-10'>
-
-        <section className='grid grid-cols-1 md:grid-cols-2 gap-2'>
-          <CardStore pathUrl='https://sallobos.cl/content/uploads/2018/07/Header-Productos-1440x800-Lobos.jpg'>El sabor de nuestro hogar desde hace mas de 100 años</CardStore>
-          <CardStore pathUrl='https://sallobos.cl/content/uploads/2020/08/Header_SaldeMar1920x600px.jpg'>Sabor profuncdo para todas tus comidas</CardStore>
-          <CardStore pathUrl='https://sallobos.cl/content/uploads/2020/09/Header_Parrillera.png'>El mejor sabor para tus parrillas</CardStore>
-          <CardStore pathUrl='https://sallobos.cl/content/uploads/2022/04/range-selecciones.jpg'>Sales de especialidad para disfrutar una experiencia en sabor</CardStore>
-        </section>
+      <div className='bg-slate-200'>
+        <Carousel/>
+        <div className='bg-slate-200 px-5 py-10'>
+          <section className='grid grid-cols-1 md:grid-cols-2 gap-2'>
+            <CardStore pathUrl='https://sallobos.cl/content/uploads/2018/07/Header-Productos-1440x800-Lobos.jpg'>El sabor de nuestro hogar desde hace mas de 100 años</CardStore>
+            <CardStore pathUrl='https://sallobos.cl/content/uploads/2020/08/Header_SaldeMar1920x600px.jpg'>Sabor profuncdo para todas tus comidas</CardStore>
+            <CardStore pathUrl='https://sallobos.cl/content/uploads/2020/09/Header_Parrillera.png'>El mejor sabor para tus parrillas</CardStore>
+            <CardStore pathUrl='https://sallobos.cl/content/uploads/2022/04/range-selecciones.jpg'>Sales de especialidad para disfrutar una experiencia en sabor</CardStore>
+          </section>
+        </div>
       </div>
 
-      <section className='flex overflow-x-auto relative bg-slte-100 px-5 py-10 md:px-24 md:py-24 gap-3 bg-slate-100'>
+      <h2 className='bg-slate-100 md:pl-72 md:pr-72 px-5 py-4 text-3xl md:pt-8'>Recetas</h2>
+      <section className='flex md:pl-72 md:pr-72 overflow-x-auto relative bg-slte-100 px-5 py-10 md:px-24 md:py-5  gap-3 bg-slate-100'>
         <CardStore2 pathUrl='https://sallobos.cl/content/uploads/bfi_thumb/empanadas-silvana-ox0l2122m4fdsfqdala0mi5mlmwtz26ueejkaynv40.jpg'/>
         <CardStore2 pathUrl='https://sallobos.cl/content/uploads/bfi_thumb/Receta_Lobos_1440x800_03-nsw50ze0pu4cnzp5lc88yy4brjsivqsqhho6e79vls.jpg'/>
         <CardStore2 pathUrl='https://sallobos.cl/content/uploads/bfi_thumb/Header-Recetas-1440x800-Lobos-Papas-con-bacon-o06v17z6qgumnhj9f959dxd3wglq2hi4lsw1n5yvy8.jpg'/>
       </section>
-      <section className='flex md:grid overflow-x-auto gap-2 px-5 py-10 md:px-24 md:py-24 grid-cols-12 md:grid-cols-3 h-auto w-full bg-slate-200'>
+      <h2 className='w-full bg-slate-200 px-5 md:pt-10 text-3xl md:pl-72 md:pr-72'>Blog</h2>
+      <section className='flex md:pl-72 md:pr-72 md:grid overflow-x-auto gap-2 px-5 py-10 md:px-24 md:py-10 grid-cols-12 md:grid-cols-3 h-auto w-full bg-slate-200'>
         <CardStore2/>
         <CardStore2/>
         <CardStore2/>
