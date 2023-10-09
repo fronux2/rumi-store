6bedf3ac9d74b288f9f6bf7c0733e640badab17f
marcos <marcosantonio.munozg@gmail.com>
2023-10-09 02:12:41 -0300

    ajuste tamaño side-bar, hidden agregado al tamaño lg para ocultar

--- src/app/components/side-bar.tsx
+++ src/app/components/side-bar.tsx
@@ -12,7 +12,7 @@ const SideBar2 = () => {
 
   return (
         <>
-          <div id="navv" className='flex h-24 w-full relative xl:pl-2 xl:gap-6 justify-between items-center'>
+          <div id="navv" className='flex xl:h-24 h-16 w-full relative xl:pl-2 xl:gap-6 justify-between items-center'>
             <a className='xl:pl-28 pl-10'>LOBOS</a>
             <ul className='lg:flex hid xl:gap-6 gap-4 text-xs font-medium'>
               <li><a href="">INICIO</a></li>
@@ -30,16 +30,16 @@ const SideBar2 = () => {
               <li><a href="" className='border-2 rounded-full py-2 px-3'>Y</a></li>
             </ul>
             <div className=''>
-              <div className='absolute z-30 top-0 right-0'>
-                <button onClick={toggleColor} className='burger flex flex-col p-1 top-3 right-3 fixed'>
-                    <div className='linea'></div>
-                    <div className='linea'></div>
-                    <div className='linea'></div>
+              <div className='flex absolute z-30 top-0 right-0'>
+                <button onClick={toggleColor} className='w-10 h-10 bg-slate-50 flex flex-col top-3 right-3 fixed gap-1 justify-center items-center lg:hidden rounded-md'>
+                    <div className='w-6 h-1 bg-orange-400'></div>
+                    <div className='w-6 h-1 bg-orange-400'></div>
+                    <div className='w-6 h-1 bg-orange-400'></div>
                 </button>
               </div>
 
               <div className={containerClassName + ' text-white pt-28'}>
-                  <ul className={'flex flex-col gap-4 pl-5'}>
+                  <ul className={'flex flex-col gap-4 pl-5 sm:justify-center sm:items-center'}>
                     <li><a href="">INICIO1</a></li>
                     <li><a href="">SAL TRADICIONAL</a></li>
                     <li><a href="">SAL PARRILLERA</a></li>
