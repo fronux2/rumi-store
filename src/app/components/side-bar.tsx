db6a0831ae8522f44b63dcbee5c25d15dc643c08
marcos <marcosantonio.munozg@gmail.com>
2023-10-09 02:32:12 -0300

    agregue z-40 al side-bar para que no se sobrepusieran las card

--- src/app/components/side-bar.tsx
+++ src/app/components/side-bar.tsx
@@ -12,7 +12,7 @@ const SideBar2 = () => {
 
   return (
         <>
-          <div id="navv" className='flex xl:h-24 h-16 w-full relative xl:pl-2 xl:gap-6 justify-between items-center'>
+          <div id="navv" className='flex xl:h-24 h-16 w-full z-40 xl:pl-2 bg-white fixed xl:gap-6 justify-between items-center'>
             <a className='xl:pl-28 pl-10'>LOBOS</a>
             <ul className='lg:flex hid xl:gap-6 gap-4 text-xs font-medium'>
               <li><a href="">INICIO</a></li>
@@ -30,8 +30,8 @@ const SideBar2 = () => {
               <li><a href="" className='border-2 rounded-full py-2 px-3'>Y</a></li>
             </ul>
             <div className=''>
-              <div className='flex absolute z-30 top-0 right-0'>
-                <button onClick={toggleColor} className='w-10 h-10 bg-slate-50 flex flex-col top-3 right-3 fixed gap-1 justify-center items-center lg:hidden rounded-md'>
+              <div className='flex absolute z-40 top-0 right-0'>
+                <button onClick={toggleColor} className='w-10 h-10 bg-slate-50 flex flex-col top-3 right-3 fixed z-40 gap-1 justify-center items-center lg:hidden rounded-md'>
                     <div className='w-6 h-1 bg-orange-400'></div>
                     <div className='w-6 h-1 bg-orange-400'></div>
                     <div className='w-6 h-1 bg-orange-400'></div>
