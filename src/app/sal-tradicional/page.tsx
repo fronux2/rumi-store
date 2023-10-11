afc7f5d4b30ffc6578053481185ce01338745ecf
marcos <marcosantonio.munozg@gmail.com>
2023-10-11 18:29:14 -0300

    se agrego responsive ala web

--- src/app/sal-tradicional/page.tsx
+++ src/app/sal-tradicional/page.tsx
@@ -70,21 +70,24 @@ export default function PageTradicional () {
         <>
           <div className="bg-black w-full h-screen flex flex-col pt-16">
             <div className="w-full h-auto bg-slate-100">
-              <div className='flex items-center justify-center w-full h-36 overflow-hidden relative '>
+              <div className='flex items-center justify-center w-full h-36 overflow-hidden relative lg:h-80'>
                 <div className='flex flex-col items-center justify-center text-white absolute z-20'>
                   <h1 className='text-3xl pb-3'>Sal Tradicional</h1>
                   <p className='text-sm'>El sabor de nuestro hogar desde hace más de 100 años</p>
                 </div>
                 <div className='absolute bg-gradient-to-t from-black z-10 w-full h-full'></div>
-                <Image src={card11} className='' alt='sal tradicional' width={2080} height={800}/>
+                <Image src={card11} className='' alt='sal tradicional' width={1440} height={800}/>
               </div>
-              <main className='grid grid-cols-2 p-5 gap-2'>
+              <main className='grid grid-cols-2 lg:grid-cols-4 p-5 gap-2 lg:px-64'>
                 {dataCard3.map(card3 => {
                   return <Card3 key={card3.titulo} pathUrl={card3.imagen} titulo={card3.titulo} descripcion={card3.descripcion} />
                 })}
               </main>
-              <section className='p-6 bg-slate-200'>
-                <Image src={img1} alt='' width={1440} height={800}/>
+              <section className='md:p-6 bg-slate-200 md:flex md:flex-row md:items-center md:h-96'>
+                <div className='md:w-[50%] md:h-auto md:pl-60 md:'>
+                  <Image src={img1} alt='' width={1440} height={800}/>
+                </div>
+
                 <div className='p-6'>
                   <h1 className='text-xl font-semibold'>Recetas</h1>
                   <p className='text-sm'>conoce las mejores recetas de Sal Lobos</p>
