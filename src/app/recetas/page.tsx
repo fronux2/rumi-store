bb84a6c9f5e86da3cce85aff8722deef82d63825
marcos <marcosantonio.munozg@gmail.com>
2023-10-13 02:30:44 -0300

    agregue componente presentacion a page recetas

--- src/app/recetas/page.tsx
+++ src/app/recetas/page.tsx
@@ -1,3 +1,11 @@
+import Presentacion from '../components/presentacion'
+import receta1 from '../imgs/recetas1.png'
 export default function Recetas () {
-
+  return (
+        <>
+            <div className="w-full pt-16">
+                <Presentacion titulo="Recetas" descripcion="Encuentra recetas ricas y caseras para disfrutar el sabor de casa." pathUrl={receta1} />
+            </div>
+        </>
+  )
 }
